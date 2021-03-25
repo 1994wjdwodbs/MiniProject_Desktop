a84a8418a156adaeeaa5db80e47b822dc356adad
JYJ_FROM_KOOKBI <1994wjdwodbs@gmail.com>
2021-03-25 17:20:06 +0900

    WPF ERP Tutorial Project

---
+++ WpfSMSApp/WpfSMSApp/Helper/Commons.cs
@@ -0,0 +1,20 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfSMSApp.Model;
+
+namespace WpfSMSApp.Helper
+{
+    public class Commons
+    {
+        // NLog 정적 인스턴스 생성
+        public static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
+
+        // Model의 User 테이블(클래스)
+        // 로그인 유저 정보
+        public static User LOGINED_USER;
+    }
+}
